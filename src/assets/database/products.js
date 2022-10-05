cab4f98989f082c59891187ef68e71c7bd030a9e
Jatou Gaye <jatougaye@gmail.com>
2022-10-05 19:35:35 +0100

    final

--- src/assets/database/products.js
+++ src/assets/database/products.js
@@ -1,4 +1,4 @@
-
+// all images imported from images directory
 import product_01_image_01 from '../images/product_01.jpg';
 import product_01_image_02 from '../images/product_01.1.jpg';
 import product_01_image_03 from '../images/product_01.3.jpg';
@@ -19,15 +19,23 @@ import product_05_image_01 from '../images/product_04.jpg';
 import product_05_image_02 from '../images/product_08.jpg';
 import product_05_image_03 from '../images/product_09.jpg';
 
-import product_06_image_01 from '../images/bread(1).png';
-import product_06_image_02 from '../images/bread(2).png';
-import product_06_image_03 from '../images/bread(3).png';
+import product_05_image_04 from '../images/quick_easy_1.jpg';
+import product_05_image_05 from '../images/quick_easy_2.jpg';
+
+import product_06_image_01 from '../images/healthy_1.png';
+import product_06_image_02 from '../images/healthy_2.png';
+import product_06_image_03 from '../images/healthy_3.png';
+import product_06_image_04 from '../images/healthy_4.jpg';
+import product_06_image_05 from '../images/healthy_5.jpg';
+import product_06_image_06 from '../images/healthy_6.jpg';
+import product_06_image_07 from '../images/healthy_7.jpg';
+import product_06_image_08 from '../images/healthy_8.jpg';
 
 const products = [
   {
     id: '01',
-    title: 'King Shrimps',
-     price: 15,
+    title: 'King Prawn',
+    price: 15,
     image01: product_01_image_01,
     image02: product_01_image_02,
     image03: product_01_image_03,
@@ -39,7 +47,7 @@ const products = [
   {
     id: '02',
     title: 'Oven-Baked Duck Ragu',
-     price: 15,
+    price: 15,
     image01: product_02_image_01,
     image02: product_02_image_02,
     image03: product_02_image_03,
@@ -51,7 +59,7 @@ const products = [
   {
     id: '03',
     title: 'Fruity Moroccan-Style Chicken',
-     price: 15,
+    price: 15,
     image01: product_03_image_01,
     image02: product_03_image_02,
     image03: product_03_image_03,
@@ -63,7 +71,7 @@ const products = [
   {
     id: '04',
     title: 'Chicken Soup',
-     price: 15,
+    price: 15,
     image01: product_04_image_01,
     image02: product_04_image_02,
     image03: product_04_image_03,
@@ -85,7 +93,7 @@ const products = [
   },
   {
     id: '06',
-    title: 'Spicy Shrimp',
+    title: 'King Prawn',
     price: 15,
     image01: product_01_image_01,
     image02: product_01_image_02,
@@ -98,7 +106,7 @@ const products = [
   {
     id: '07',
     title: 'Saucy Gochujang Chicken',
-     price: 15,
+    price: 15,
     image01: product_02_image_02,
     image02: product_02_image_01,
     image03: product_02_image_03,
@@ -109,7 +117,7 @@ const products = [
 
   {
     id: '08',
-    title: 'Thin Cheese Quick_and_Easy',
+    title: 'Zingy Salad',
     price: 15,
     image01: product_03_image_02,
     image02: product_03_image_01,
@@ -121,7 +129,7 @@ const products = [
 
   {
     id: '09',
-    title: 'Quick_and_Easy With Mushroom',
+    title: 'Rice with Fruit',
     price: 15,
     image01: product_04_image_02,
     image02: product_04_image_01,
@@ -178,6 +186,91 @@ const products = [
 
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.',
   },
+
+  {
+    id: '14',
+    title: 'Salmon En Papillote',
+    price: 15,
+    image01: product_06_image_04,
+    image02: product_06_image_05,
+    image03: product_06_image_08,
+    category: 'Healty_Choices',
+
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.',
+  },
+
+  {
+    id: '15',
+    title: 'Veggie Balti',
+    price: 15,
+    image01: product_06_image_05,
+    image02: product_06_image_06,
+    image03: product_06_image_07,
+    category: 'Healty_Choices',
+
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.',
+  },
+
+  {
+    id: '16',
+    title: 'Cottage Pie',
+    price: 15,
+    image01: product_06_image_06,
+    image02: product_06_image_07,
+    image03: product_06_image_08,
+    category: 'Healty_Choices',
+
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.',
+  },
+
+  {
+    id: '17',
+    title: 'Courgette Lasagne',
+    price: 15,
+    image01: product_06_image_07,
+    image02: product_06_image_08,
+    image03: product_06_image_01,
+    category: 'Healty_Choices',
+
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.',
+  },
+
+  {
+    id: '18',
+    title: 'Jumbo Prawn',
+    price: 15,
+    image01: product_06_image_08,
+    image02: product_06_image_01,
+    image03: product_06_image_02,
+    category: 'Healty_Choices',
+
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.',
+  },
+
+  {
+    id: '19',
+    title: 'Haddock & Tomato',
+    price: 15,
+    image01: product_05_image_04,
+    image02: product_05_image_03,
+    image03: product_05_image_05,
+    category: 'Quick_and_Easy',
+
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.',
+  },
+
+  {
+    id: '20',
+    title: 'Tomato Salad',
+    price: 15,
+    image01: product_05_image_05,
+    image02: product_05_image_04,
+    image03: product_05_image_03,
+    category: 'Quick_and_Easy',
+
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.',
+  },
+
 ];
 
 export default products;
